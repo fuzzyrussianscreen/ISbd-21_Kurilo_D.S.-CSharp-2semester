d828a0914a73bedd8687c744cdc7458af9609992
OkCur64 OkCur64 <ok.curilo2010@yandex.ru>
2019-04-06 14:13:14 +0400

    1

---
+++ PizzeriaServiceDAL/Interfaces/IReptService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaServiceDAL.Interfaces
+{
+    public class IReptService
+    {
+        void SavePizzaPrice(ReptBindingModel model);
+        List<StoragesLoadViewModel> GetStoragesLoad();
+        void SaveStoragesLoad(ReptBindingModel model);
+        List<CustomerIndentViewModel> GetCustomerIndents(ReptBindingModel model);
+        void SaveCustomerIndents(ReptBindingModel model);
+    }
+}
